bccce49c489631aefe4baec2a555b5b827677b67
dropecosta <dropecosta@gmail.com>
2018-07-25 16:20:27 -0300

    evolving creditList component

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { reduxForm, Field } from 'redux-form'
+import { reduxForm, Field, formValueSelector } from 'redux-form'
 
 import { init } from './billingCycleActions'
 import LabelAndInput from '../common/form/labelAndInput'
@@ -10,14 +10,14 @@ import CreditList from './creditList'
 class BillingCycleForm extends Component {
 
     render() {
-        const { handleSubmit, readOnly } = this.props
+        const { handleSubmit, readOnly, credits } = this.props
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
                     <Field name='name' component={LabelAndInput} readOnly={readOnly} label='Nome' cols='12 4' placeholder='Informe o nome' />
                     <Field name='month' component={LabelAndInput} readOnly={readOnly} type='number' label='Mês' cols='12 4' placeholder='Informe o mês' /> <Field name='year' component={LabelAndInput} readOnly={readOnly} type='number' label='Ano' cols='12 4' placeholder='Informe o ano' />
                     
-                    <CreditList cols='12 6' readOnly={readOnly}/>
+                    <CreditList cols='12 6' list={credits} readOnly={readOnly}/>
 
                 </div>
                 <div className='box-footer'>
@@ -30,5 +30,7 @@ class BillingCycleForm extends Component {
 }
 
 BillingCycleForm = reduxForm({form: 'billingCycleForm', destroyOnUnmount: false})(BillingCycleForm)
+const selector = formValueSelector('billingCycleForm')
+const mapStateToProps = state => ({credits: selector(state, 'credits')})
 const mapDispatchToProps = dispatch => bindActionCreators({init}, dispatch) 
-export default connect(null, mapDispatchToProps)(BillingCycleForm)
+export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleForm)
