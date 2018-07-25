55a2bffb97b04d7222d0f52b3fdd5eb271e1a8a9
dropecosta <dropecosta@gmail.com>
2018-07-25 17:07:40 -0300

    generalizing CreditList component

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -5,7 +5,7 @@ import { reduxForm, Field, formValueSelector } from 'redux-form'
 
 import { init } from './billingCycleActions'
 import LabelAndInput from '../common/form/labelAndInput'
-import CreditList from './creditList'
+import ItemList from './itemList'
 
 class BillingCycleForm extends Component {
 
@@ -17,7 +17,7 @@ class BillingCycleForm extends Component {
                     <Field name='name' component={LabelAndInput} readOnly={readOnly} label='Nome' cols='12 4' placeholder='Informe o nome' />
                     <Field name='month' component={LabelAndInput} readOnly={readOnly} type='number' label='Mês' cols='12 4' placeholder='Informe o mês' /> <Field name='year' component={LabelAndInput} readOnly={readOnly} type='number' label='Ano' cols='12 4' placeholder='Informe o ano' />
                     
-                    <CreditList cols='12 6' list={credits} readOnly={readOnly}/>
+                    <ItemList cols='12 6' list={credits} readOnly={readOnly} field='credits' legend='Créditos'/>
 
                 </div>
                 <div className='box-footer'>
